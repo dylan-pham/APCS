68c15079d4b0f7b95097d977d73f97c938461e8c
Dylan Pham <43.dpham@gmail.com>
2019-03-07 18:12:22 -0800

    implemented color palette

--- Labs/#20 Pixel Art Lab/ColorPalette.java
+++ Labs/#20 Pixel Art Lab/ColorPalette.java
@@ -1,15 +1,15 @@
 import java.awt.Graphics;
 import java.awt.Color;
  
-public class Square {
+public class ColorPalette {
     private int red;
     private int green;
     private int blue;
 
-    public Square(int red, int green, int blue) {
+    public ColorPalette(int red, int green, int blue) {
         this.red = red;
         this.green = green;
-        this.blue = blue;
+        this.blue = blue;   
     }
 
     public void drawMe(Graphics g, int x, int y) {
@@ -18,11 +18,5 @@ public class Square {
 
         g.setColor(Color.BLACK);
         g.drawRect(x, y, 20, 20);
-    }
-
-    public void changeColor(int red, int green, int blue) {
-        this.red = red;
-        this.green = green;
-        this.blue = blue;
     }
 }
